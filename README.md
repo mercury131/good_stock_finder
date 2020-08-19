344b928d31621731ff8e813bb3d7d98aee86b5a3
darkwind <mercury131@ya.ru>
2020-08-19 19:39:27 +0300

    add  Good stock finder

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # good_stock_finder
-good stock search engine
+This program is not an individual investment recommendation and cannot be used as a robot advisor.
+
+This program search not bad stocks from S&P 500 Index.
+
+# How does search work?
+The program selects stocks according to the following parameters:
+1. The company has sales revenue for the last quarter
+2. The company has no big debts
+3. The ratio is not overheated
+4. Good ROE/Gross Margin/Profit Margin
+
+# How to use it?
+
+First install python 3 and this components via pip:
+1. yum install python36 (Centos) or apt install python3 (Debian\Ubuntu) or get installer for Windows [here](https://www.python.org/downloads/windows/)
+2. pip install pandas
+3. pip install finviz
+
+Then open directory with good_stock_finder.py and run it!
+`python good_stock_finder.py`
+
+The program will show the found stocks:
+`Processing ticker -  WMT
+Processing ticker -  WY
+Processing ticker -  WMB
+Processing ticker -  XEL
+Processing ticker -  XLNX
+Processing ticker -  ZBRA
+Processing ticker -  ZTS
+Found Good Tickers -  ['ABT', 'ATVI', 'AMD', 'AAP', 'APD', 'AKAM', 'ALB', 'ARE', 'ALGN', 'GOOGL', 'AEE', 'AWK', 'ABC', 'APH', 'ADI', 'ANTM', 'AOS', 'AMAT', 'APTV', 'ADM', 'AJG', 'BIO', 'BLK', 'BWA', 'BMY', 'BR', 'CHRW', 'CDNS', 'CLX', 'CTSH', 'CPRT', 'GLW', 'COST', 'CSX', 'DVA', 'DXCM', 'DLR', 'DPZ', 'DOV', 'DRE', 'ETFC', 'ETN', 'EBAY', 'EW', 'EA', 'EQIX', 'EL', 'FB', 'FIS', 'FRC', 'FMC', 'FTV', 'FBHS', 'GIS', 'HOLX', 'IDXX', 'ITW', 'IR', 'IQV', 'JNJ', 'JCI', 'KSU', 'K', 'KEYS', 'KLAC', 'LH', 'LRCX', 'LEN', 'MKTX', 'MMC', 'MA', 'MXIM', 'MCHP', 'MSFT', 'MDLZ', 'MCO', 'MS', 'MSCI', 'NDAQ', 'NEM', 'NSC', 'NVR', 'ORLY', 'ORCL', 'PCAR', 'PH', 'PYPL', 'PNR', 'PKI', 'PFE', 'PG', 'PGR', 'PLD', 'PHM', 'QCOM', 'REGN', 'RHI', 'ROP', 'CRM', 'SBAC', 'NOW', 'SWKS', 'SWK', 'STE', 'SIVB', 'SNPS', 'TROW', 'TTWO', 'TFX', 'TIF', 'TT', 'UNP', 'UNH', 'VAR', 'VRSN', 'VRSK', 'VZ', 'V', 'WMT', 'WY', 'WMB', 'XEL', 'XLNX', 'ZBRA', 'ZTS']
+Found Hot Tickers -  []`
+
+The program also exports data to a CSV files in current directory.
---
+++ good_stock_finder.py
@@ -0,0 +1,176 @@
+print("Start Search..")
+
+import pandas as pd
+
+import csv
+
+import os
+
+from finviz.screener import Screener
+import finviz
+
+setprice = input('Do you want set max price for ticker? (Y\\N): ') 
+
+if setprice == "Y"  :
+    maxprice = float(input("Enter max price value: ") )
+    print("Set max price to:", maxprice) 
+elif setprice == "y"  :
+    maxprice = float(input("Enter max price value: ") )
+    print("Set max price to:", maxprice)
+else :
+    print("Max price not set..")
+
+# There are 2 tables on the Wikipedia page
+# we want the first table
+
+payload=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+first_table = payload[0]
+second_table = payload[1]
+
+df = first_table
+
+symbols = df['Symbol'].values.tolist()
+
+#print(symbols)
+
+found_tickers = []
+    
+for ticker in symbols :
+    print("Get data for ticker - ", ticker)
+    curprice = float((finviz.get_stock(ticker.replace(".","-"))['Price']))
+    range52 = (finviz.get_stock(ticker.replace(".","-"))['52W Range'])
+    low52 = float((range52.split('-')[0].strip()))
+    high52 = float((range52.split('-')[1].strip()))
+    try:
+        ticker_radar = round((high52 - low52) / (high52 - curprice))
+    except ZeroDivisionError:
+        ticker_radar = 0
+    if (ticker_radar > 4) and (ticker_radar < 30) :
+        try:
+            if curprice > maxprice :
+                print("Skip, Ticker by price - ", ticker)
+            else:
+                found_tickers.append(ticker)
+        except NameError:
+            found_tickers.append(ticker)
+
+
+
+good_tickers = []
+hot_tickers = []
+
+
+
+for ticker in found_tickers :
+    print("Processing ticker - ", ticker)
+    try:
+        quickratio = float((finviz.get_stock(ticker.replace(".","-"))['Quick Ratio']))
+    except ValueError:
+        quickratio = 5
+    try:
+        currentratio = float((finviz.get_stock(ticker.replace(".","-"))['Current Ratio']))
+    except ValueError:
+        currentratio = 5
+    try:
+        debteq = float((finviz.get_stock(ticker.replace(".","-"))['Debt/Eq']))
+    except ValueError:
+        debteq = 100
+    try:
+        salesqq = float((finviz.get_stock(ticker.replace(".","-"))['Sales Q/Q']).replace("%",""))
+    except ValueError:
+        salesqq = 0
+    try:
+        grossmargin = float((finviz.get_stock(ticker.replace(".","-"))['Gross Margin']).replace("%",""))
+    except ValueError:
+        grossmargin = 0
+    try:
+        profitmargin = float((finviz.get_stock(ticker.replace(".","-"))['Profit Margin']).replace("%",""))
+    except ValueError:
+        profitmargin = 0
+    try:
+        roe = float((finviz.get_stock(ticker.replace(".","-"))['ROE']).replace("%",""))
+    except ValueError:
+        roe = 0
+    if (quickratio < 1) :
+        quickratio = 2
+    elif (quickratio > 2) :
+        quickratio = 3
+    elif (quickratio > 1) :
+        quickratio = 5
+
+    if (currentratio < 2) :
+        currentratio = 3
+    elif (currentratio < 1) :
+        currentratio = 2
+    elif (currentratio > 2) :
+        currentratio = 5
+
+    if (debteq <= 1) :
+        debteq = 5
+    elif (debteq > 2) :
+        debteq = 2
+    elif (debteq < 2) :
+        debteq = 3
+
+    if (salesqq > 0.1) :
+        salesqq = 5
+    elif (salesqq < 0) :
+        salesqq = 2
+    elif (salesqq == 0) :
+        salesqq = 3
+    elif (salesqq > 0) :
+        salesqq = 4
+
+    if (grossmargin > 0.4) :
+        grossmargin = 5
+    elif (grossmargin < 0.1) :
+        grossmargin = 2
+    elif (grossmargin == 0) :
+        grossmargin = 1
+    elif (grossmargin > 0.3) :
+        grossmargin = 4
+
+    if (profitmargin > 0.4) :
+        profitmargin = 5
+    elif (profitmargin < 0.1) :
+        profitmargin = 2
+    elif (profitmargin == 0) :
+        profitmargin = 1
+    elif (profitmargin < 0.3) :
+        profitmargin = 3
+    elif (profitmargin > 0.3) :
+        profitmargin = 4
+
+    if (roe > 0.5) :
+        roe = 5
+    elif (roe < 0.1) :
+        roe = 2
+    elif (roe == 0) :
+        roe = 1
+    elif (roe < 0.3) :
+        roe = 3
+    elif (roe > 0.3) :
+        roe = 4
+    finalresult = round(quickratio + currentratio + debteq + salesqq + grossmargin + profitmargin + roe)
+
+    if (finalresult <= 24) :
+        tempvar=0
+    elif (finalresult > 24) :
+        good_tickers.append(ticker)
+    elif (finalresult > 30) :
+        hot_tickers.append(ticker)
+
+
+print("Found Good Tickers - ", good_tickers)
+
+print("Found Hot Tickers - ", hot_tickers)
+
+for ticker in good_tickers :
+    with open((os.path.dirname(os.path.realpath(__file__)) + '/good_tickers.csv'), 'a', newline='') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(ticker)
+
+for ticker in hot_tickers :
+    with open((os.path.dirname(os.path.realpath(__file__)) + '/hot_tickers.csv'), 'a', newline='') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(ticker)
